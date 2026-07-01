83d0dede8da6a3692abefc9797e0fbd26254826a
Gopala <ggopala1904@gmail.com>
2026-07-01 17:31:50 +0530

    feat: add dotenv support and Docker configuration

--- perfomace-optimization/load-balancing.js
+++ perfomace-optimization/load-balancing.js
@@ -1,14 +1,15 @@
 import express from 'express';
-const PORT = process.env.PORT || 5500;
+import dotenv from 'dotenv';
+
 
 const app = express();
 
+dotenv.config();
 
 app.get("/", (req, res) => {
     res.status(200).json({"running get request from Docker": "success"});
 })
 
-
-app.listen(PORT,() =>{
-    console.log(`Server is running on Docker port ${PORT}`);
+app.listen(process.env.PORT,() =>{
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
