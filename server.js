81d68bafb95fc08012fd767ded74f77fb5d87f48
Gopala <ggopala1904@gmail.com>
2026-07-13 16:11:27 +0530

    feat: integrate ioredis for Redis support and update landing page content

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from "url";
+import redis from "./config/redis.config.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,6 +11,22 @@ const PORT = 3000;
 const replicaApp_Name = process.env.APP_NAME
 
 const app = express();
+
+redis.set("mykey", "MyValue From NodeJs");
+
+// ioredis supports the node.js callback style
+redis.get("mykey", (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(result); // Prints "value"
+  }
+});
+
+// Or ioredis returns a promise if the last argument isn't a function
+redis.get("mykey").then((result) => {
+  console.log(result); // Prints "value"
+});
 
 
 app.get("/", (req, res) => {
